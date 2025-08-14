b9803d1693fb307cadd600c03faa31ef6231ddba
Pedro Henrique Guimarães <202507077491@alunos.estacio.br>
2025-08-14 12:49:58 +0000

    final

--- xadrez.c
+++ xadrez.c
@@ -1,29 +1,77 @@
 #include <stdio.h>
 
-int main(){
-    int torre;
-    int bispo;
-    int rainha;
+const int TORRESCASAS = 5;
+const int BISPOCASAS = 8;
+const int RAINHACASAS = 8;
 
-    // Mover a Torre 5 casas para a direita
-        printf("::::Torre::::\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n"); // Imprime a direção do movimento
+const int CAVALOVERTICAL = 2;
+const int CAVALOHORIZONTAL = 1;
+
+// Funções recursivas
+void moverTorre(int casas) {
+    if (casas > 0) {
+        printf("Direita\n");
+        moverTorre(casas - 1);
     }
+}
 
-    // Mover o Bispo 5 casas para cima e direita
-
-        printf("::::Bispo::::\n");
-    for (int i = 0; i < 5; i++){
-        printf("Cima, Direita\n");
+void moverBispo(int casas) {
+    if (casas > 0) {
+        printf("Cima\n");
+        printf("Direita\n");
+        moverBispo(casas - 1);
     }
+}
 
-    // Mover Rainha 8 casas para a esquerda
-        printf("::::Rainha::::\n");
-    for (int i = 0; i < 8; i++){
+void moverRainha(int casas) {
+    if (casas > 0) {
         printf("Esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
+
+// Função do Cavalo
+void moverCavalo() {
+    int movimentoCompleto = 1;
+    while (movimentoCompleto--) { 
+        for (int i = 0; i < CAVALOVERTICAL; i++)
+            printf("Cima\n");
+        for (int j = 0; j < CAVALOHORIZONTAL; j++)
+            printf("Direita\n");
+    }
+    printf("Cima\nBaixo\nEsquerda\nDireita\n");
+}
+
+int main() {
+    int escolha;
+
+    printf("Escolha a peça para mover:\n");
+    printf("1. Torre\n");
+    printf("2. Bispo\n");
+    printf("3. Rainha\n");
+    printf("4. Cavalo\n");
+    printf("Opção: ");
+    scanf("%d", &escolha);
+
+    printf("\n:::: Movimento da peça escolhida ::::\n");
+
+    switch (escolha) {
+        case 1:
+            moverTorre(TORRESCASAS);
+            break;
+        case 2:
+            moverBispo(BISPOCASAS);
+            break;
+        case 3:
+            moverRainha(RAINHACASAS);
+            break;
+        case 4:
+            moverCavalo();
+            break;
+        default:
+            printf("Opção inválida!\n");
+            break;
     }
 
     return 0;
 }
-                    
